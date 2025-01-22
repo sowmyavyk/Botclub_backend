fa744b1dd5cab6d2a425a43f635dd1cea60cd02d
sowmyavyk <vyakaranamsowmya8@gmail.com>
2025-01-22 14:32:48 +0530

    bracket hate

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def profile():
 
 if __name__ == "__main__":
     # Ensure the app binds to the correct port
-    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5002))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5002)))
