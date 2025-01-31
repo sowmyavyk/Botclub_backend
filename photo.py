3da7195452ace3d67c21999d82442056d7e7c93c
sowmyavyk <vyakaranamsowmya8@gmail.com>
2025-01-31 12:01:43 +0530

    chnage

--- photo.py
+++ photo.py
@@ -62,5 +62,5 @@ def fetch_image():
 
     return send_file(io.BytesIO(image_data.read()), mimetype="image/jpeg")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5001)))
