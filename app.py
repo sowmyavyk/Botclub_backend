50c5c05d4aefae8d982cdc063c7cf771f2c86f5b
sowmyavyk <vyakaranamsowmya8@gmail.com>
2025-02-05 17:09:01 +0530

    enoughh!!!!!!!

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 from flask import send_file
 import uuid  # To generate unique API keys
 from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -181,7 +182,10 @@ def update_profile():
     users_collection.update_one({"api_key": api_key}, {"$set": update_data})
 
     return jsonify({"message": "Profile updated successfully!", **update_data}), 200
-
+@app.route('/static/uploads/<filename>')
+def serve_uploaded_file(filename):
+    """Serve the uploaded profile pictures."""
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 if __name__ == "__main__":
     # Ensure the app binds to the correct port
     app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5002)))
